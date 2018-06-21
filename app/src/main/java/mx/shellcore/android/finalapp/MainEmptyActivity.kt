11943d4859203af487b2f38eab264e41e935c84a
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2018-06-21 13:27:48 -0500

    Se implementó manejador de flujo de acuerdo a si el usuario se encuentra logueado o no.

---
+++ app/src/main/java/mx/shellcore/android/finalapp/MainEmptyActivity.kt
@@ -0,0 +1,29 @@
+package mx.shellcore.android.finalapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import mx.shellcore.android.finalapp.ui.login.ui.LoginActivity
+import mx.shellcore.android.finalapp.utils.goToActivity
+
+class MainEmptyActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (mAuth.currentUser == null) {
+            goToActivity<LoginActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
+        } else {
+            goToActivity<MainActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
+        }
+
+        finish()
+    }
+}
