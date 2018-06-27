24c4dab49c8da8d7d6e1ad94528bb6ea78ad644d
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2018-06-27 15:16:01 -0500

    Implementación de Logout

--- app/src/main/java/mx/shellcore/android/finalapp/ui/main/ui/MainActivity.kt
+++ app/src/main/java/mx/shellcore/android/finalapp/ui/main/ui/MainActivity.kt
@@ -1,16 +1,21 @@
 package mx.shellcore.android.finalapp.ui.main.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
 import com.example.mylibrary.ToolbarActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import mx.shellcore.android.finalapp.R
+import mx.shellcore.android.finalapp.ui.login.ui.LoginActivity
 import mx.shellcore.android.finalapp.ui.main.ui.adapters.PagerAdapter
 import mx.shellcore.android.finalapp.ui.main.ui.fragments.ChatFragment
 import mx.shellcore.android.finalapp.ui.main.ui.fragments.InfoFragment
 import mx.shellcore.android.finalapp.ui.main.ui.fragments.RatesFragment
+import mx.shellcore.android.finalapp.utils.goToActivity
 
 class MainActivity : ToolbarActivity() {
 
@@ -24,6 +29,24 @@ class MainActivity : ToolbarActivity() {
 
         setupViewPager(getPagerAdapter())
         setupBottomNavigationBar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                FirebaseAuth.getInstance()
+                        .signOut()
+                goToActivity<LoginActivity> {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getPagerAdapter(): PagerAdapter {
